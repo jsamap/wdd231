01d55e7262a56a12359937e0f3532b8ff4972bbe
Jared Samayoa <jsamayoapenagos@byupathway.edu>
2025-11-30 02:27:34 -0600

    W04: Chamber Membership Join Page [v4]

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -1,15 +1,26 @@
+// LOAD TIMESTAMP TO HIDDEN ELEMENT
+const now = new Date().toISOString(); 
+document.getElementById("timestamp").value = now;
+
+
 // SUBMIT FORM
 document.getElementById("submit").onclick = function () {
 
     const fname = document.getElementById("fname");
+    const lname = document.getElementById("lname");
     const email = document.getElementById("email");
+    const phone = document.getElementById("phone");
     const organization = document.getElementById("org-name");
+    const timestamp = document.getElementById("timestamp");
 
-    console.log(`Form Parameters: - fname "${fname}" - email "${email}" - organization "${organization}"`);
+    console.log(`[JOIN] Form Parameters: - fname "${fname}" - lname "${lname}" - email "${email}" - phone "${phone}" - organization "${organization}" - timestamp "${timestamp}"`);
 
     localStorage.setItem("fname-cc", fname.value);
+    localStorage.setItem("lname-cc", lname.value);
     localStorage.setItem("email-cc", email.value);
+    localStorage.setItem("phone-cc", phone.value);
     localStorage.setItem("organization-cc", organization.value);
+    localStorage.setItem("timestamp-cc", timestamp.value);
 };
 
 
--- chamber/scripts/thankyou.js
+++ chamber/scripts/thankyou.js
@@ -1,9 +1,26 @@
 // FNAME AND DNAME IN MESSAGE
 const fname = window.localStorage.getItem("fname-cc");
+const lname = window.localStorage.getItem("lname-cc");
 const email = window.localStorage.getItem("email-cc");
+const phone = window.localStorage.getItem("phone-cc");
 const organization = window.localStorage.getItem("organization-cc");
+const timestamp = window.localStorage.getItem("timestamp-cc");
 
-console.log(`Form Parameters: - fname "${fname}" - email "${email}" - organization "${organization}"`);
+console.log(`[THANKYOU] Form Parameters: - fname "${fname}" - lname "${lname}" - email "${email}" - phone "${phone}" - organization "${organization}" - timestamp "${timestamp}"`);
 
 const message = document.getElementById("message");
-message.innerHTML = `Hi <b>${fname}</b>! <br><br>Thank you for filling the Membership Application form. We look forward to have <b>${organization}</b> as part of our Chamber of Commerce. <br><br>We will contact with you soon at the email address <b>${email}</b>.`;
+message.innerHTML = `
+Hi <b>${fname}</b>! 
+<br><br>Thank you for filling the Membership Application form. We look forward to have <b>${organization}</b> as part of our Chamber of Commerce. 
+<br><br>The following information has been stored in our database (${timestamp}):
+<br><br>
+<ul>
+    <li>First name: ${fname}</li>
+    <li>Last name: ${lname}</li>
+    <li>Email: ${email}</li>
+    <li>Phone: ${phone}</li>
+    <li>Organization: ${organization}</li>
+    <li></li>
+</ul>
+<br>We will contact you soon at the email address <b>${email}</b>.
+`;
