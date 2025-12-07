e75b00809b56e379f901b3d04939f26358b0cb70
Jared Samayoa <jsamayoapenagos@byupathway.edu>
2025-12-07 02:09:53 -0600

    W05: Chamber Discover Page [v3]

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,6 +1,6 @@
+//   CREATE ELEMENTS BASED ON DATA
 import {places} from '../data/members.mjs';
 
-//   CREATE ELEMENTS BASED ON DATA
 const displayPlaces = (place) => {
     places.forEach((place) => {
 
@@ -26,3 +26,32 @@ const displayPlaces = (place) => {
     });
 }
 displayPlaces();
+
+// SET MESSAGE FOR LAST VISIT
+const lastVisitElement = document.querySelector('#lastVisit p');
+const lastVisit = window.localStorage.getItem("lastvisit-cc");
+const today = new Date();
+
+if (lastVisit == null) {
+    lastVisitElement.textContent = 'Welcome! Let us know if you have any questions.';
+    localStorage.setItem("lastvisit-cc", today);
+} else {
+    const lastVisitDate = new Date(lastVisit);
+    
+    const daysDiff = Math.floor((today - lastVisitDate) / (1000 * 60 * 60 * 24));
+    if (daysDiff <= 1) {
+        lastVisitElement.textContent = 'Back so soon! Awesome!';
+    } else {
+        lastVisitElement.textContent = `You last visited ${daysDiff} days ago.`;
+
+    }
+}
+
+
+// LAST VISIT MODAL - OPEN / CLOSE
+const visitContainer = document.getElementById("lastVisit");
+
+document.getElementById("visitClose").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.querySelector("main").removeChild(visitContainer);    
+});
